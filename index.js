74de34c768dd8307c062c0341e94c369a60dc1ca
constanzabonicalzi <constanzabonicalzi@gmail.com>
2021-03-28 19:19:17 -0300

    BQA-08: exercise 1 Variables y Operadores

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('Executing external script made with JS');
+
+// 1) Variables y Operadores
+
+/* a) Crear dos variables numéricas y utilizar el operador suma
+para guardar el valor de la suma de ambos números en una 3er variable. */
+
+var sum = 10 + 20;
+
+console.log('sum 10 + 20', sum);
+
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var courseA, courseB, totalStudents;
+
+courseA = 10;
+courseB = 20;
+
+totalStudents = courseA + courseB;
+
+console.log('totalStudents', sum);
+
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length). */
+
+var x = 'Safari';
+var y = 'GoogleChrome';
+
+var totalCharacters= (x.length) + (y.length)
+
+console.log('totalCharacters', totalCharacters)
