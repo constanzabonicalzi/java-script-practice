3edec35d758276dc9cb663df6fccbd2bf40f53a8
constanzabonicalzi <constanzabonicalzi@gmail.com>
2021-03-28 19:42:19 -0300

    BQA-08: exercise 2 Strings

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 console.log('Executing external script made with JS');
 
-// 1) Variables y Operadores
+//1) VARIABLES Y OPERADORES
 
-/* a) Crear dos variables numéricas y utilizar el operador suma
+/*A) Crear dos variables numéricas y utilizar el operador suma
 para guardar el valor de la suma de ambos números en una 3er variable. */
 
 var sum = 10 + 20;
-
 console.log('sum 10 + 20', sum);
 
-// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+//B) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
 var courseA, courseB, totalStudents;
-
 courseA = 10;
 courseB = 20;
-
 totalStudents = courseA + courseB;
-
 console.log('totalStudents', sum);
 
-/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
-guardando el resultado la suma una 3er variable (utilizar length). */
+/*C) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado la suma una 3er variable (utilizar length)*/
 
 var x = 'Safari';
 var y = 'GoogleChrome';
+var totalCharacters= (x.length) + (y.length);
+console.log('totalCharacters', totalCharacters);
 
-var totalCharacters= (x.length) + (y.length)
+// 2) STRINGS
 
-console.log('totalCharacters', totalCharacters)
+/*A) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula
+(utilizar toUpperCase)*/
+
+var title = 'Introduction of Static Websites';
+var title01 = title.toUpperCase();
+console.log('Title', title01);
